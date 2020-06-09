1089a3ac8d000ef475dbf0ec140568e896537a16
Dane <dnaniel213@gmail.com>
2020-06-09 19:33:27 +0800

    - Added test client side

--- bsg/BSG.Blockchain.Proximax.Portal/Program.cs
+++ bsg/BSG.Blockchain.Proximax.Portal/Program.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using BSG.Blockchain.Portal.Proximax;
-using BSG.Blockchain.Models;
-using Newtonsoft.Json;
+
 namespace proximax_gamedev_rest
 {
     public class Program
@@ -19,16 +17,12 @@ namespace proximax_gamedev_rest
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
                 });
 
         public static void Main(string[] args)
         {
-            var content = JsonConvert.SerializeObject(portal.GenerateWallet(1));
 
-            var buffer = System.Text.Encoding.UTF8.GetBytes(content);
-            var byteContent = new ByteArrayContent(buffer);
-            var response = httpclient.PostAsync("https://localhost:5001/api/Accounts", byteContent);
-            Console.WriteLine(response.IsCompletedSuccessfully);
             CreateHostBuilder(args).Build().Run();
         }
  
