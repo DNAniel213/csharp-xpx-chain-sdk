019b63a641c77aeb77ebccda9db7ac4ccc7ffe68
rsapa <ranzsapa@gmail.com>
2020-06-13 11:19:42 +0800

    [Prototype.ProximaxNamespace] Create Namespace & Subnamespace, Read Namespace
    [Prototype.Program] Added CreateNamespaceTest & CreateSubNamespaceTest functions

--- bsg/Xarcade.Api.Prototype/Program.cs
+++ bsg/Xarcade.Api.Prototype/Program.cs
@@ -2,6 +2,8 @@
 using XarcadeAccount = Xarcade.Api.Prototype.Blockchain.Models;
 using System.Reactive.Linq;
 using Xarcade.Api.Prototype.Blockchain;
+using ProximaX.Sirius.Chain.Sdk.Model.Namespaces;
+
 namespace Xarcade.Api.Prototype
 {
     class Program
@@ -9,17 +11,23 @@ namespace Xarcade.Api.Prototype
         public ProximaxBlockchainPortal portal = null;
         public ProximaxAccount pAccount = null;
         public ProximaxMosaic pMosaic = null;
+        public ProximaxNamespace pNamespace = null;
         public const string TEST_PRIVATE_KEY_DANE1 = "580B37D8481BDEA4DD1BEA8098EF006AE46DD654FBBC0903418478C1FA363F15"; //Dane 1's account
         public const string TEST_PRIVATE_KEY_DANE2 = "169598F9678F1BE5D959DB8CED4F8FA2297A5124A241E034DC38AD7A0F41B724"; //Dane 2's account
+        public const string TEST_PRIVATE_KEY = "90B02ADCFF8D7776EF0016537EBC25709CD1A7F8317CAB3A84DB8FEED52460DC"; //Bruh account
+        public const string namespaceName = "suhdude";
         static void Main(string[] args)
         {
             Program p = new Program();
             p.portal = new ProximaxBlockchainPortal();
             p.pAccount = new ProximaxAccount(p.portal);
             p.pMosaic = new ProximaxMosaic(p.portal);
+            p.pNamespace = new ProximaxNamespace(p.portal);
 
             //p.CreateMosaicTest();
-            p.SendMosaicTest();
+            //p.SendMosaicTest();
+            p.CreateNamespace();
+            //p.CreateSubNamespace();
         }
         
         //Creates a mosaic using Dane's private key
@@ -43,6 +51,36 @@ namespace Xarcade.Api.Prototype
 
             var sendCoinT = pMosaic.SendCoin(mosaicInfo, dane1, dane2.Address, 20, "hope this works.jpg");
             portal.SignAndAnnounceTransaction(dane1, sendCoinT).GetAwaiter().GetResult();
+        }
+
+        //Creates a namespace using Wah's Private Key
+        private void CreateNamespace()
+        {
+            var account = pAccount.CreateAccount(1, TEST_PRIVATE_KEY);
+
+            try
+            {
+                var namespaceInfo = pNamespace.GetNamespaceInformation(namespaceName).GetAwaiter().GetResult();
+                throw new ArgumentNullException($"Namespace already exists!");
+            }
+            catch (Flurl.Http.FlurlHttpException)
+            {
+                var registerNamespace = pNamespace.CreateNamespace(namespaceName, 100);
+                portal.SignAndAnnounceTransaction(account, registerNamespace).GetAwaiter().GetResult();
+            }
+        }
+
+        //Creates a subnamespace using Wah's private key and Parent namespace
+        private void CreateSubNamespace()
+        {
+            var account = pAccount.CreateAccount(1, TEST_PRIVATE_KEY);
+
+            var subNamespaceName = "wahwah";
+            var namespaceId = new NamespaceId(subNamespaceName);
+            var parentId = new NamespaceId(namespaceName);
+
+            var registerSubNamespace = pNamespace.CreateSubNamespace(subNamespaceName, parentId);
+            portal.SignAndAnnounceTransaction(account, registerSubNamespace).GetAwaiter().GetResult();
         }
     }
 }
--- bsg/Xarcade.Api.Prototype/Proximax/ProximaxNamespace.cs
+++ bsg/Xarcade.Api.Prototype/Proximax/ProximaxNamespace.cs
@@ -1,7 +1,12 @@
+using ProximaX.Sirius.Chain.Sdk.Model.Namespaces;
+using ProximaX.Sirius.Chain.Sdk.Model.Transactions;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
 namespace Xarcade.Api.Prototype.Blockchain
 {
     /// <summary>
-    /// Contains all methods involving an Namespace
+    /// Contains all methods involving a Namespace
     /// </summary>
     public class ProximaxNamespace
     {
@@ -10,9 +15,48 @@ namespace Xarcade.Api.Prototype.Blockchain
         {
             this.portal = portal;
         }
+        
+        /// <summary>
+        /// Registers a new Namespace
+        /// </summary>
+        /// <param name="parentNamespaceName">The name of the namespace </param>
+        /// <param name="duration">The maximum rent duration of the namespace </param>
+        /// <returns></returns>
+        public RegisterNamespaceTransaction CreateNamespace(string parentNamespaceName, ulong duration)
+        {
+            var registerNamespace = RegisterNamespaceTransaction.CreateRootNamespace(
+                Deadline.Create(),
+                parentNamespaceName,
+                duration,
+                portal.networkType);
 
-//TODO Create Namespace
-//TODO Read Namespace
+            return registerNamespace;
+        }
+
+        /// <summary>
+        /// Registers a new sub Namespace
+        /// </summary>
+        /// <param name="namespaceName">The name of the subnamespace </param>
+        /// <param name="parentNamespaceId">The name of the parent namespace </param>
+        /// <returns></returns>
+        public RegisterNamespaceTransaction CreateSubNamespace (string namespaceName, NamespaceId parentNamespaceId)
+        {
+            var registerSubNamespace = RegisterNamespaceTransaction.CreateSubNamespace(
+                Deadline.Create(),
+                namespaceName,
+                parentNamespaceId,
+                portal.networkType
+            );
+
+            return registerSubNamespace;
+        }
+        public async Task<NamespaceInfo> GetNamespaceInformation (string namespaceName)
+        {
+            NamespaceInfo namespaceInfo = await portal.siriusClient.NamespaceHttp.GetNamespace(new NamespaceId(namespaceName));
+
+            return namespaceInfo;
+        }
+    
 //TODO Update Namespace
 //TODO Delete Namespace
     }
