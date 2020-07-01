8973b8441c60131ae43dd16fd034c886ee2fd11b
john son <jvson10@gmail.com>
2020-07-01 17:49:23 +0800

    [GenerateRandomString]GenerateRandom

--- bsg/Xarcade.Infrastructure/Cryptography/DecryptionServices.cs
+++ bsg/Xarcade.Infrastructure/Cryptography/DecryptionServices.cs
@@ -4,7 +4,8 @@ using System.Convert;
 namespace Xarcade.Api.Prototype.Cryptography
 {
     public class DecryptionServices
-    {        static public string DecodeFrom64(string encodedData)
+    {        
+        static public string DecodeFrom64(string encodedData)
         {
             byte[] toEncodeDataAsBytes = System.Convert.FromBase64String(encodedData);
             string returnValue = System.Text.ASCIIEncoding.ASCII.GetString(toEncodeAsBytes);
