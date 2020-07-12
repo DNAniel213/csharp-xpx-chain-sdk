0a92c64938fb214b2395840bcfae6be5f7a7ea00
Janyl Lambee Tanoy <tanoyjl@gmail.com>
2020-07-13 00:03:29 +0800

    XAR-407 Xarcade.Application.Models Create Dto Models for Account, Token, and Transaction

---
+++ bsg/Xarcade.Application/Xarcade/Models/Account/OwnerDto.cs
@@ -0,0 +1,9 @@
+using System;
+namespace Xarcade.Application.Xarcade.Models.Account
+{
+    /// <summary>Xarcade Application Layer Owner Model</summary>
+    public class OwnerDto
+    {
+        
+    }
+}
