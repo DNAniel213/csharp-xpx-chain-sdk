c15e703448d4d59d15185c4e0daab9e2bdce5758
Janyl Lambee Tanoy <tanoyjl@gmail.com>
2020-07-14 19:35:06 +0800

    XARA-408 Implement Interface IDataAccessProximaX

---
+++ bsg/Xarcade.Infrastructure/Repository/IDataAccessProximaX.cs
@@ -0,0 +1,19 @@
+﻿using Xarcade.Domain.ProximaX;
+using Xarcade.Domain.Authentication;
+
+namespace Xarcade.Infrastructure.Repository
+{
+    interface IDataAccessProximaX
+    {
+
+        public bool SaveOwner(Owner ownerDTO);
+        public bool SaveUser(User userDTO);
+        public bool SaveNamespace(Namespace namespaceDTO);
+        public bool SaveMosaic(Mosaic mosaicDTO);
+        public bool SaveXar(Domain.ProximaX.Xarcade xarcadeDTO);
+        public bool SaveTransaction(Transaction transactionDTO);
+        public bool SaveXarcadeUser(XarcadeUser xarUserDTO);
+        public Owner LoadOwner(long userID);
+        public User LoadUser(long userID);
+    }
+}
