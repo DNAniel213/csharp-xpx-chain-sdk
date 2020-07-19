6ab182510ab9abdcc901cd0ac4d6dda0ae800735
Janyl Lambee Tanoy <tanoyjl@gmail.com>
2020-07-19 08:49:00 +0800

    XARA-404 Related Added documentation (params and summaries)

--- bsg/Xarcade.Application/Xarcade/TransactionService.cs
+++ bsg/Xarcade.Application/Xarcade/TransactionService.cs
@@ -1,8 +1,10 @@
-using System;
+using System.Threading.Tasks;
 using Xarcade.Infrastructure.Abstract;
 using Xarcade.Infrastructure.Repository;
 using Xarcade.Infrastructure.Utilities.Logger;
-
+using Xarcade.Application.Xarcade.Models.Account;
+using Xarcade.Application.Xarcade.Models.Token;
+using Xarcade.Application.Xarcade.Models.Transaction;
 
 namespace Xarcade.Application.Xarcade
 {
@@ -17,6 +19,29 @@ namespace Xarcade.Application.Xarcade
         {
             this.dataAccessProximaX = dataAccessProximaX;
             this.blockchainPortal = blockchainPortal;
+        }
+        /// <summary>
+        /// Sends a custom token from user to another user
+        /// </summary>
+        public async Task<TokenTransactionDto> SendTokenAsync(TokenDto token,AccountDto sender, AccountDto receiver)
+        {
+            
+            return await Task.FromResult<TokenTransactionDto>(null);
+        }
+
+        /// <summary>
+        /// Sends a Xar token from user to another user
+        /// </summary>
+        public async Task<TokenTransactionDto> SendXarAsync(TokenDto token,AccountDto sender, AccountDto receiver)
+        {
+            return await Task.FromResult<TokenTransactionDto>(null);
+        }
+        /// <summary>
+        /// Sends a Xpx token from user to another user
+        /// </summary>
+        public async Task<TokenTransactionDto> SendXpxAsync(TokenDto token,AccountDto sender, AccountDto receiver)
+        {
+            return await Task.FromResult<TokenTransactionDto>(null);
         }
     }
 
