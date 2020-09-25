30183d6d5772782654a6f928e21d2010361647e0
rsapa <ranzsapa@gmail.com>
2020-09-25 14:50:06 +0800

    login, token POST integration

---
+++ Xarcade.Dashboard/assets/js/api-dashboard/_script.login.js
@@ -0,0 +1,30 @@
+const loginURI     = 'http://localhost:5000/xarcadeaccount/login';
+const redirectPage = '../api-dashboard/dashboard.html';
+
+function loginUser()
+{
+    const loginUserInput     = document.getElementById('username');
+    const loginPasswordInput = document.getElementById('password');
+
+    const item = {
+        username: loginUserInput.value,
+        password: loginPasswordInput.value,
+    }
+
+    fetch(loginURI, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(item),
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data['message'] === 'Ok'){
+                localStorage.setItem('userData', JSON.stringify(data['authenticationData']['account']));
+                localStorage.setItem('token', JSON.stringify(data['authenticationData']['jwtToken']));
+                localStorage.setItem('cookie', JSON.stringify(data['authenticationData']['refreshToken']));
+                redirect: window.location.replace(redirectPage);
+            }else{
+                alert(data['message']);
+            }
+        });
+}
