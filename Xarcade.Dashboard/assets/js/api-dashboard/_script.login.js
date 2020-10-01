342d442d588561a54a92398463607a826de76efd
rsapa <ranzsapa@gmail.com>
2020-10-01 20:53:19 +0800

    create owner integration

--- Xarcade.Dashboard/assets/js/api-dashboard/_script.login.js
+++ Xarcade.Dashboard/assets/js/api-dashboard/_script.login.js
@@ -1,5 +1,8 @@
 const loginURI = 'http://localhost:5000/xarcadeaccount/login';
 
+const getOwnerURI    = 'http://localhost:5000/account/owner';
+const createOwnerURI = 'http://localhost:5000/account/generate/owner';
+
 const redirectPage = '../api-dashboard/dashboard.html';
 
 function loginUser()
@@ -22,9 +25,61 @@ function loginUser()
             if (data['message'] === 'Ok'){
                 localStorage.setItem('userData', JSON.stringify(data['authenticationData']['account']));
                 localStorage.setItem('jwtToken', JSON.stringify(data['authenticationData']['jwtToken']));
-                redirect: window.location.replace(redirectPage);
+
+                getOwner();
             }else{
                 alert(data['message']);
             }
         });
+}
+
+function getOwner()
+{
+    let userData = JSON.parse(localStorage.getItem('userData'));
+    let jwtToken = JSON.parse(localStorage.getItem('jwtToken'));
+
+    let params = new URLSearchParams({
+        userId: userData.userId,
+        searchId: userData.userId
+    });
+
+    fetch(getOwnerURI + '?' + params.toString(), {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + jwtToken,
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if(response['status'] === 204){
+                alert('Creating Owner!');
+                createOwner();
+            }else{
+                redirect: window.location.replace(redirectPage);
+            }
+        });
+}
+
+function createOwner()
+{
+    let userData = JSON.parse(localStorage.getItem('userData'));
+    let jwtToken = JSON.parse(localStorage.getItem('jwtToken'));
+
+    let params = new URLSearchParams({
+        userId: userData.userId
+    });
+
+    fetch(createOwnerURI + '?' + params.toString(),{
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + jwtToken,
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            if(data['message'] === 'Success!'){
+                redirect: window.location.replace(redirectPage);
+            }
+        })
 }
